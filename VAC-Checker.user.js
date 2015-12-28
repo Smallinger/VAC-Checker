a66e9e0f55fac8ecf1ad4a702cdef66e6180cc65
M.Schirrmann <info@colordesigns.de>
2015-12-28 04:27:33 +0100

    Update VAC-Checker.user.js
    Test Update functions :D

--- VAC-Checker.user.js
+++ VAC-Checker.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         VAC-Checker
-// @namespace    https://scriptr.org
-// @version      0.1
+// @namespace    github.com/Junxx/VAC-Checker
+// @version      0.2
 // @description  Easily see VAC bans on players you've played with in the past.
 // @author       SmallPox
 // @Credits      MrHayato
