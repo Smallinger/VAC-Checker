9df7037d38ae72d8a7e04c32dce03c252fbab091
M.Schirrmann <info@colordesigns.de>
2015-12-28 04:21:46 +0100

    Create VAC-Checker.meta.js

---
+++ VAC-Checker.meta.js
@@ -0,0 +1,6 @@
+// ==UserScript==
+// @name        Update and Download URL Metadata VAC-Checker
+// @version     0.1
+// @updateURL   https://github.com/Junxx/VAC-Checker/VAC-Checker.meta.js
+// @downloadURL https://github.com/Junxx/VAC-Checker/VAC-Checker.user.js
+// ==/UserScript==
